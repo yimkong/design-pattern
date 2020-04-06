f248b63bc242294c61f2fbb953a8fa807c37861f
yimkong <17665448577@163.com>
2020-04-06 21:13:13 +0800

    add note

--- src/main/java/decorator/CondimentDecorator.java
+++ src/main/java/decorator/CondimentDecorator.java
@@ -2,7 +2,7 @@ package decorator;
 
 /**
  * author yg
- * description 调料
+ * description 调料 抽象装饰者
  * date 2020/4/6
  */
 public abstract class CondimentDecorator extends Beverage{
--- src/main/java/decorator/Main.java
+++ src/main/java/decorator/Main.java
@@ -1,12 +1,14 @@
 package decorator;
 
+import java.io.IOException;
+
 /**
  * author yg
  * description 装饰者模式：动态地将责任附加到对象上，若要拓展功能，装饰者提供了比继承更有弹性的替代方案
  * date 2020/4/6
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //双倍摩卡奶油咖啡
         Beverage beverage = new Espresso();
         System.out.println(beverage.getDescription() + " cost:" + beverage.cost());
