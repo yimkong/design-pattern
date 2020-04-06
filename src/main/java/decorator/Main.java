5b14ff6e82fc453bf468f7851325a9cab1260f80
yimkong <17665448577@163.com>
2020-04-06 21:34:31 +0800

    add note

--- src/main/java/decorator/Main.java
+++ src/main/java/decorator/Main.java
@@ -9,7 +9,7 @@ import java.io.IOException;
  */
 public class Main {
     public static void main(String[] args) throws IOException {
-        //双倍摩卡奶油咖啡
+        //双倍摩卡奶油浓缩咖啡
         Beverage beverage = new Espresso();
         System.out.println(beverage.getDescription() + " cost:" + beverage.cost());
         beverage = new Mocha(beverage);
